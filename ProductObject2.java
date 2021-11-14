3364874644250132f2365ed19c141ecdaf8b42c1
Irvan Naufali Rahmanto <irvan.rahmanto123@gmail.com>
2021-11-14 21:38:54 +0700

    Kode Program No.14
    Kode Program No.14

---
+++ ProductObject2.java
@@ -0,0 +1,12 @@
+package productobject;
+
+public class ProductObject2 {
+    
+     public static void main(String[] args) {         
+//         Mendefinisikan Object baru yaitu myProduct pada class ProductObject2
+         ProductObject2 myProduct = new ProductObject2();         
+         
+//         Mencetak Object baru myProduct serta mendeklarasikan Type nya
+         System.out.println(myProduct.getClass());
+     }
+}
